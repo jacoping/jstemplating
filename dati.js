701c4c6b3d75f3732a241584f1077acdbffe95f4
jacoping <jacoping@gmail.com>
2020-03-29 23:32:19 +0200

    hello world
    messo su un sito di prova con dati sia sul sito che su un array di progetti

---
+++ dati.js
@@ -0,0 +1,22 @@
+var dati = {
+  website:
+    {
+      title: "il mio portfolio",
+      subtitle: "sito di prova utilizzando mustache.js",
+      logo: "logo.png"
+    },
+progetti: [
+  {
+    title: "progetto 1",
+    description: "lorem ipsum"
+},
+  {
+    title: "progetto 2",
+    description: "lorem ipsum"
+  },
+  {
+    title: "progetto 3",
+    description: "lorem ipsum"
+  }
+]
+  }
