3a3716dead78bfca68a7cae10bbce9f739aa6503
jacoping <jacoping@gmail.com>
2020-04-01 23:10:27 +0200

    add pics in works

--- dati.js
+++ dati.js
@@ -5,18 +5,36 @@ var dati = {
       subtitle: "sito di prova utilizzando mustache.js",
       logo: "logo.png"
     },
-progetti: [
+works: [
   {
     title: "progetto 1",
-    description: "lorem ipsum"
+    description: "lorem ipsum <strong> sid </strong>",
+    cover: "image1.jpg",
+    pics: [
+      "image1.jpg",
+      "image2.jpg",
+      "image3.jpg"
+    ]
 },
   {
     title: "progetto 2",
-    description: "lorem ipsum"
+    description: "lorem ipsum",
+    cover: "image1.jpg",
+    pics: [
+      "image1.jpg",
+      "image2.jpg",
+      "image3.jpg"
+    ]
   },
   {
     title: "progetto 3",
-    description: "lorem ipsum"
-  }
+    description: "lorem ipsum",
+    cover: "image1.jpg",
+    pics: [
+      "image1.jpg",
+      "image2.jpg",
+      "image3.jpg"
+    ]
+      }
 ]
   }
